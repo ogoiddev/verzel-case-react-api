09be31e964fd1f15dd289957860dc270cf1e285f
Diogo <diogompantaleao@gmail.com>
2022-11-01 13:02:19 -0300

    chore: error middleware preparado. Zod instalado. tsconfig definido

--- src/app.ts
+++ src/app.ts
@@ -6,15 +6,12 @@ import * as swaggerUi from 'swagger-ui-express';
 
 import helmet from 'helmet';
 import errorHandler from './middleware/errorMiddleware';
-import Routes from './routes/index';
 import swaggerFile from './swaggerApi.json';
 
 const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-
-app.use(Routes);
 
 app.use(errorHandler);
 
