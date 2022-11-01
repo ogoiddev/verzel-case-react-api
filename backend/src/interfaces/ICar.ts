ec39f1ab509a1fcfe21e5b503447cc519d994c1b
Diogo <diogompantaleao@gmail.com>
2022-11-01 16:18:40 -0300

    feat: Interface for Car Entity defined

--- backend/src/interfaces/ICar.ts
+++ backend/src/interfaces/ICar.ts
@@ -3,7 +3,7 @@ import { VehicleZodSchema } from './IVehicle';
 
 export const CarZodSchema = z.intersection(VehicleZodSchema, z.object({
   doorsQty: z.number().min(2).max(4),
-  seatsQty: z.number().min(2).max(7),
+  seatsQty: z.number().min(2).max(9),
 }));
 
 export type ICar = z.infer<typeof CarZodSchema>;
--- backend/src/interfaces/IVehicle.ts
+++ backend/src/interfaces/IVehicle.ts
@@ -3,9 +3,16 @@ import { z } from 'zod';
 export const VehicleZodSchema = z.object({
   model: z.string().min(3),
   year: z.number().min(1900).max(2022),
-  color: z.string().min(3),
+  color: z.string(),
   status: z.boolean().optional(), 
   buyValue: z.number().int(),
+  imgs: z.array(z.string()),
+  features: z.object({
+    state: z.string().max(20),
+    city: z.string().max(30),
+    km: z.number().max(6),
+    gearShift: z.string().max(10),
+  }),
 });
   
 export type IVehicle = z.infer<typeof VehicleZodSchema>;
