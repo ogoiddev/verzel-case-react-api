29388a0d610f1ce192eaec838a2bf2288d988fd0
ogoiddev <diogompantaleao@gmail.com>
2022-11-04 01:11:54 -0300

    deploy: connect changed

--- backend/src/models/connection.ts
+++ backend/src/models/connection.ts
@@ -1,10 +1,12 @@
+/* eslint-disable max-len */
 import 'dotenv/config';
 import mongoose from 'mongoose';
 
-const MONGO_DB_URL = 'mongo "mongodb://mongo:ckfjGEiIyS4VbmQcyqW0@containers-us-west-99.railway.app:7682"';
+// const MONGO_DB_URL = 'mongo "mongodb://mongo:ckfjGEiIyS4VbmQcyqW0@containers-us-west-99.railway.app:7682"';
 
 const connectToDatabase = (
-  mongoDatabaseURI = process.env.MONGO_URI, 
-) => mongoose.connect(mongoDatabaseURI || MONGO_DB_URL);
+  mongoDatabaseURI = process.env.MONGO_URI,
+  mongoDatabaseURL = process.env.MONGO_DB_URL,
+) => mongoose.connect(mongoDatabaseURL || mongoDatabaseURI || '');
 
 export default connectToDatabase;
