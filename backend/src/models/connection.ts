7f4575f793ccd64a8b0c48ecb3834f852d75c4cb
Diogo <93263797+ogoiddev@users.noreply.github.com>
2022-11-04 00:33:21 -0300

    change to deploy

--- backend/src/models/connection.ts
+++ backend/src/models/connection.ts
@@ -1,10 +1,10 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 
-// const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
+const MONGO_DB_URL = 'mongo "mongodb://mongo:ckfjGEiIyS4VbmQcyqW0@containers-us-west-99.railway.app:7682"';
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI, 
-) => mongoose.connect(mongoDatabaseURI || '');
+) => mongoose.connect(mongoDatabaseURI || MONGO_DB_URL);
 
 export default connectToDatabase;
