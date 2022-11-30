c92dd4285bedacdb18d16552c79377bee204980f
ogoiddev <diogompantaleao@gmail.com>
2022-11-30 16:07:54 -0300

    chore: url railway for connection

--- backend/src/models/connection.ts
+++ backend/src/models/connection.ts
@@ -3,10 +3,10 @@ import 'dotenv/config';
 import mongoose from 'mongoose';
 
 const MONGO_DB_URL = 'mongodb://mongodb:27017/CarShop';
-// const MONGO = 'mongodb://mongo:ckfjGEiIyS4VbmQcyqW0@containers-us-west-99.railway.app:7682';
+const MONGO = 'mongodb://mongo:1pr1qIahWGXESXxvz5sT@containers-us-west-99.railway.app:7682';
 
 const connectToDatabase = (
   mongoDatabaseURL = process.env.MONGO_DB_URL,
-) => mongoose.connect(mongoDatabaseURL || MONGO_DB_URL);
+) => mongoose.connect(mongoDatabaseURL || MONGO || MONGO_DB_URL);
 
 export default connectToDatabase;
